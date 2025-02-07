6ee451cf7fcdf3ce416d230b96983e62eff1d695
Hasan <sohagali.ru.ac@gmail.com>
2025-02-07 22:37:45 +0600

    add logo and favicon

---
+++ src/component/loading/Loading.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Spinner } from "@heroui/spinner";
+
+const Loading = () => {
+  return (
+    <div className="bg-white/10 fixed inset-0 z-[999] backdrop-blur-md flex justify-center items-center">
+      <Spinner />;
+    </div>
+  );
+};
+
+export default Loading;
